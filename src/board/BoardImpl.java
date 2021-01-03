ecef8b3445b5dcd60dcbdeca6b1a89cdadaa0ad3
Mitchell Gray <meg346@cornell.edu>
2021-01-02 20:44:57 -0500

    Test cases and fixed boardimpl to fit spec

--- src/board/BoardImpl.java
+++ src/board/BoardImpl.java
@@ -1,8 +1,8 @@
 package board;
 
-import pieces.PieceFactory;
 import pieces.Piece.Color;
 import pieces.Piece.Type;
+import pieces.PieceFactory;
 
 /** Implementation of the chess board */
 public class BoardImpl extends AbstractBoard implements Board {
@@ -14,26 +14,26 @@ public class BoardImpl extends AbstractBoard implements Board {
 	 * Creates an instance of a chess board 
 	 */
 	public BoardImpl() { 
-		board[0][0] = factory.getPiece(Type.ROOK, Color.BLACK);
-		board[0][1] = factory.getPiece(Type.KNIGHT, Color.BLACK);
-		board[0][2] = factory.getPiece(Type.BISHOP, Color.BLACK);
-		board[0][3] = factory.getPiece(Type.QUEEN, Color.BLACK);
-		board[0][4] = factory.getPiece(Type.KING, Color.BLACK);
-		board[0][5] = factory.getPiece(Type.BISHOP, Color.BLACK);
-		board[0][6] = factory.getPiece(Type.KNIGHT, Color.BLACK);
-		board[0][7] = factory.getPiece(Type.ROOK, Color.BLACK);
+		board[0][0] = factory.getPiece(Type.ROOK, Color.WHITE);
+		board[0][1] = factory.getPiece(Type.KNIGHT, Color.WHITE);
+		board[0][2] = factory.getPiece(Type.BISHOP, Color.WHITE);
+		board[0][3] = factory.getPiece(Type.QUEEN, Color.WHITE);
+		board[0][4] = factory.getPiece(Type.KING, Color.WHITE);
+		board[0][5] = factory.getPiece(Type.BISHOP, Color.WHITE);
+		board[0][6] = factory.getPiece(Type.KNIGHT, Color.WHITE);
+		board[0][7] = factory.getPiece(Type.ROOK, Color.WHITE);
 		for (int i = 0; i < board[1].length; i++)
-			board[1][i] = factory.getPiece(Type.PAWN, Color.WHITE);
-		board[7][0] = factory.getPiece(Type.ROOK, Color.WHITE);
-		board[7][1] = factory.getPiece(Type.KNIGHT, Color.WHITE);
-		board[7][2] = factory.getPiece(Type.BISHOP, Color.WHITE);
-		board[7][3] = factory.getPiece(Type.KING, Color.WHITE);
-		board[7][4] = factory.getPiece(Type.QUEEN, Color.WHITE);
-		board[7][5] = factory.getPiece(Type.BISHOP, Color.WHITE);
-		board[7][6] = factory.getPiece(Type.KNIGHT, Color.WHITE);
-		board[7][7] = factory.getPiece(Type.ROOK, Color.WHITE);
+			board[1][i] = factory.getPiece(Type.PAWN, Color.BLACK);
+		board[7][0] = factory.getPiece(Type.ROOK, Color.BLACK);
+		board[7][1] = factory.getPiece(Type.KNIGHT, Color.BLACK);
+		board[7][2] = factory.getPiece(Type.BISHOP, Color.BLACK);
+		board[7][3] = factory.getPiece(Type.KING, Color.BLACK);
+		board[7][4] = factory.getPiece(Type.QUEEN, Color.BLACK);
+		board[7][5] = factory.getPiece(Type.BISHOP, Color.BLACK);
+		board[7][6] = factory.getPiece(Type.KNIGHT, Color.BLACK);
+		board[7][7] = factory.getPiece(Type.ROOK, Color.BLACK);
 		for (int i = 0; i < board[6].length; i++)
-			board[6][i] = factory.getPiece(Type.PAWN, Color.BLACK);
+			board[6][i] = factory.getPiece(Type.PAWN, Color.WHITE);
 		
 	}
 }
