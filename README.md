9bee29c092841f374edae71500a9699eeb12cc83
Mitchell <r2d2mg200@gmail.com>
2021-07-08 23:23:03 -0400

    removed everything

--- README.md
+++
@@ -1,3 +0,0 @@
-# Chess
-It's chess
-(Backend-only so far. Intended to become Android app)
--- app/src/main/java/com/robotchad/chess/client/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-/** The controller of the chess game */
-public interface Controller {
-
-	//TODO
-	
-}
