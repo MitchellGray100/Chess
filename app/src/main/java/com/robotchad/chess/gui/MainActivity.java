9b158dc0963a1ba1168742709c895c957df75b8d
Jacob Kerr <jckawesome13@gmail.com>
2021-07-27 23:17:19 -0400

    Added dynamically created button on top of image

--- app/src/main/java/com/robotchad/chess/gui/MainActivity.java
+++ app/src/main/java/com/robotchad/chess/gui/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,10 +19,13 @@ import com.robotchad.chess.client.board.BoardImpl;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
 
     /** The instance of the board used for all game activities */
     private Board board;
@@ -31,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         board = new BoardImpl(); // Create new board instance
         setContentView((R.layout.chessboard));
-
+        RelativeLayout ll = findViewById(R.id.mainLayout);
+        //System.out.println(ll);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        Button myButton = new Button(this);
+        myButton.setText("Push Me");
+        ll.addView(myButton, lp);
+        //myButton.bringToFront();
+        //findViewById(R.id.boardImageView).
     }
 }
