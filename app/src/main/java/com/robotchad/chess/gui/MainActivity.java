64bce98a4e031483c054327d4cdd5828b2f9b6b5
Jacob Kerr <jckawesome13@gmail.com>
2021-08-22 09:07:30 -0400

    Added autogenerated grid of chess squares

--- app/src/main/java/com/robotchad/chess/gui/MainActivity.java
+++ app/src/main/java/com/robotchad/chess/gui/MainActivity.java
@@ -16,10 +16,15 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.robotchad.chess.client.board.Board;
 import com.robotchad.chess.client.board.BoardImpl;
+import com.robotchad.chess.client.controller.Controller;
+import com.robotchad.chess.client.controller.ControllerImpl;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -27,20 +32,37 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** The instance of the board used for all game activities */
-    private Board board;
+    /** The instance of the chess controller used for all game activities */
+    private Controller controller;
+    /** The 2D array of buttons making up the chess board in the GUI */
+    private ImageButton[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        board = new BoardImpl(); // Create new board instance
+        controller = new ControllerImpl(); // Create new board instance
+        buttons = new ImageButton[8][8];
         setContentView((R.layout.chessboard));
-        RelativeLayout ll = findViewById(R.id.mainLayout);
+        GridLayout ll = findViewById(R.id.boardLayout);
         //System.out.println(ll);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        Button myButton = new Button(this);
-        myButton.setText("Push Me");
-        ll.addView(myButton, lp);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                ImageButton myButton = new ImageButton(this);
+                int imageID;
+                if ((i + j) % 2 == 0) {
+                    imageID = R.drawable.white_square;
+                } else {
+                    imageID = R.drawable.black_square;
+                }
+                myButton.setImageResource(imageID);
+                myButton.setBackgroundColor(R.color.transparent);
+                //myButton.setColorFilter(R.color.transparent);
+                buttons[i][j] = myButton;
+                ll.addView(myButton);
+                myButton.getLayoutParams().height = ll.getHeight() / 8 - 2;
+                myButton.getLayoutParams().width = ll.getWidth() / 8 - 2;
+            }
+        }
         //myButton.bringToFront();
         //findViewById(R.id.boardImageView).
     }
