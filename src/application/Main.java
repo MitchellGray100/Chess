f7069c42a607a9300f64c8d7f869407cfadc10ee
Mitchell <r2d2mg200@gmail.com>
2021-09-11 00:16:35 -0400

    Changed run configurations to work

--- src/application/Main.java
+++ src/application/Main.java
@@ -52,7 +52,7 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 
 		primaryStage.show();
 		primaryStage.setScene(new Scene(createContent()));
@@ -62,4 +62,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
